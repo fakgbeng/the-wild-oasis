b5eab63901d03c193157f9a622eb35a17cdf9eaa
Gbenga Fakorede <temris123@gmail.com>
2023-10-05 16:48:27 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+    <GlobalStyles/>
+    <StyledApp>
+     <Heading as='h1'>The Wild Oasis</Heading>
+     <Heading as='h2'>Check in and out</Heading>
+     <Button onClick={() => alert("Check In")}>Check In</Button>
+      <Button onClick={() => alert("Check Out")}>Check Out</Button>
+      <Heading as='h3'>Form</Heading>
+      <Input type="number" placeholder="Number of guests"/>
+      <Input type="number" placeholder="Number of guests"/>
+      </StyledApp>
+      </>
   )
 }
 
-export default App
+export default App;
